17eaa8eae7524d49b931f8b18dd2c53e18554b4b
Attia Fouad <104443707+Attia-Fouad@users.noreply.github.com>
2023-07-08 20:12:26 +0300

    finish home item design

--- lib/View/home/components/build_home_item.dart
+++ lib/View/home/components/build_home_item.dart
@@ -65,12 +65,15 @@ Widget lastPartOfHomeItem() => Container(
         ),
       ),
     );
+
 Widget bigContainerChild() {
   return Column(
     children: [
       firstPartOfContainer(),
       secondPartOfContainer(),
       thirdPartOfContainer(),
+      fourthPartOfContainer(),
+      fifthPartOfContainer(),
     ],
   );
 }
@@ -174,9 +177,271 @@ Widget thirdPartOfContainer() {
             const SizedBox(
               height: 6,
             ),
+            Text(
+              AppStrings.priceInSaudi,
+              style: Styles.textStyle14Bold.copyWith(
+                color: HexColor('#2E2E2E'),
+              ),
+            ),
+            const SizedBox(
+              height: 17,
+            ),
+            Text(
+              AppStrings.analysisType,
+              style: Styles.textStyle12Medium.copyWith(
+                color: containerTextColor,
+              ),
+            ),
+            const SizedBox(
+              height: 6,
+            ),
+            Text(
+              AppStrings.speculation,
+              style: Styles.textStyle14Bold.copyWith(
+                color: HexColor('#2E2E2E'),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              'وقف الخسارة',
+              style: Styles.textStyle12Medium.copyWith(
+                color: HexColor('#515151'),
+              ),
+            ),
+            const SizedBox(
+              height: 6,
+            ),
+            Text(
+              '12.8',
+              style: Styles.textStyle14Bold.copyWith(
+                color: HexColor('#515151'),
+              ),
+            ),
+          ],
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              AppStrings.targetPrice,
+              style: Styles.textStyle12Medium.copyWith(
+                color: containerTextColor,
+              ),
+            ),
+            const SizedBox(
+              height: 6,
+            ),
+            Text(
+              AppStrings.priceInSaudi,
+              style: Styles.textStyle14Bold.copyWith(
+                color: HexColor('#2E2E2E'),
+              ),
+            ),
+            const SizedBox(
+              height: 17,
+            ),
+            Text(
+              'نسبة التغير في السعر',
+              style: Styles.textStyle12Medium.copyWith(
+                color: containerTextColor,
+              ),
+            ),
+            const SizedBox(
+              height: 6,
+            ),
+            Text(
+              '100%',
+              style: Styles.textStyle14Bold.copyWith(
+                color: HexColor('#4EB48E'),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              'التغير لسعر اليوم',
+              style: Styles.textStyle12Medium.copyWith(
+                color: HexColor('#515151'),
+              ),
+            ),
+            const SizedBox(
+              height: 6,
+            ),
+            Text(
+              '1%-',
+              style: Styles.textStyle14Bold.copyWith(
+                color: HexColor('#EA3F3F'),
+              ),
+            ),
+          ],
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              AppStrings.todayPrice,
+              style: Styles.textStyle12Medium.copyWith(
+                color: containerTextColor,
+              ),
+            ),
+            const SizedBox(
+              height: 6,
+            ),
+            Text(
+              '473,411.32',
+              style: Styles.textStyle14Bold.copyWith(
+                color: HexColor('#2E2E2E'),
+              ),
+            ),
+            const SizedBox(
+              height: 17,
+            ),
+            Text(
+              'مدة التحليل',
+              style: Styles.textStyle12Medium.copyWith(
+                color: containerTextColor,
+              ),
+            ),
+            const SizedBox(
+              height: 6,
+            ),
+            Text(
+              'يوم 1',
+              style: Styles.textStyle14Bold.copyWith(
+                color: HexColor('#2E2E2E'),
+              ),
+            ),
           ],
         ),
       ],
     ),
+  );
+}
+
+Widget fourthPartOfContainer() {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.spaceAround,
+    children: [
+      Container(
+        height: 32,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          color: HexColor('#FAFAFA'),
+        ),
+        child: Center(
+          child: Row(
+            children: [
+              Icon(
+                Icons.check_circle_outline_outlined,
+                color: customOrangeColor,
+              ),
+              const SizedBox(
+                width: 4,
+              ),
+              Text(
+                'ناجحة',
+                style: Styles.textStyle13Medium.copyWith(
+                  color: HexColor('#6F6BB2'),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      Container(
+        height: 32,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          color: HexColor('#FAFAFA'),
+        ),
+        child: Center(
+          child: Row(
+            children: [
+              Icon(
+                Icons.sports_volleyball_outlined,
+                color: HexColor('#6F6BB2'),
+              ),
+              const SizedBox(
+                width: 4,
+              ),
+              Text(
+                'شركة ريت للأبحاث',
+                style: Styles.textStyle13Medium.copyWith(
+                  color: HexColor('#6F6BB2'),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      Container(
+        height: 32,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          color: HexColor('#FAFAFA'),
+        ),
+        child: Center(
+          child: Row(
+            children: [
+              Icon(
+                Icons.cloud_done_outlined,
+                color: HexColor('#6F6BB2'),
+              ),
+              const SizedBox(
+                width: 4,
+              ),
+              Text(
+                'قطاع الأعمال',
+                style: Styles.textStyle13Medium.copyWith(
+                  color: HexColor('#6F6BB2'),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    ],
+  );
+}
+
+Widget fifthPartOfContainer() {
+  return Row(
+    children: [
+      Expanded(
+          child: Container(
+        height: 40,
+        color: Colors.blue.withOpacity(0.2),
+        child: Center(
+          child: Icon(
+            Icons.notifications_none_outlined,
+            color: HexColor('#6F6BB2'),
+          ),
+        ),
+      )),
+      Expanded(
+          child: Container(
+        height: 40,
+        color: Colors.blue.withOpacity(0.1),
+        child: Center(
+          child: Icon(
+            Icons.thumb_up_alt_outlined,
+            color: HexColor('#6F6BB2'),
+          ),
+        ),
+      )),
+      Expanded(
+          child: Container(
+        height: 40,
+        color: Colors.blue.withOpacity(0.2),
+        child: Center(
+          child: Icon(
+            Icons.ios_share_sharp,
+            color: HexColor('#6F6BB2'),
+          ),
+        ),
+      )),
+    ],
   );
 }
